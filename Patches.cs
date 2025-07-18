5f2b2ad51a9b049a9d6b3c2e7ad56233e955556c
dre <mynameismolotov@gmail.com>
2025-07-17 21:50:55 -0400

    Add project files.

---
+++ Patches.cs
@@ -0,0 +1,83 @@
+﻿#if   Il2Cpp
+using Il2CppScheduleOne.DevUtilities;
+using Il2CppScheduleOne.Messaging;
+using Il2CppScheduleOne.PlayerScripts;
+using Il2CppScheduleOne.GameTime;
+using Il2CppScheduleOne.UI.Phone.Messages;
+
+#elif Mono
+using ScheduleOne.DevUtilities;
+using ScheduleOne.Messaging;
+using ScheduleOne.PlayerScripts;
+using ScheduleOne.GameTime;
+using ScheduleOne.UI.Phone.Messages;
+#endif
+
+using HarmonyLib;
+using MelonLoader;
+
+namespace Timestamp
+{
+    [HarmonyPatch(typeof(Player), "SleepStart")]
+    public class SleepStartPatch
+    {
+        static void Prefix() => TimestampTexts.DaySent.Clear();
+    }
+
+    [HarmonyPatch(typeof(MessagesApp), "SetCurrentConversation")]
+    public class SetCurrentConversationPatch
+    {
+        static void Prefix(MSGConversation conversation)
+        {
+            string contact   = conversation?.contactName;
+            bool showDay     = TimestampTexts.ShowDayNamePref.Value;
+            bool alreadySent = TimestampTexts.DaySent.TryGetValue(contact, out _);
+
+            if (!showDay || alreadySent || conversation is null || contact is null)
+                return;
+
+            string day = NetworkSingleton<TimeManager>.Instance.CurrentDay.ToString();
+            if (TimestampTexts.ShowDayNumPref.Value)
+                day += ", Day " + NetworkSingleton<TimeManager>.Instance.ElapsedDays;
+
+            TimestampTexts.DaySent  .Add(contact);
+            TimestampTexts.BlockMove.Add(contact);
+
+            TimestampTexts.SilenceMessage();
+            conversation.SendMessage(new Message(day, Message.ESenderType.Other), notify:false, network:false);
+        }
+    }
+
+    [HarmonyPatch(typeof(MSGConversation), "SendMessage")]
+    public class SendMessagePatch
+    {
+        static void Prefix(Message message, bool notify = true, bool network = true)
+        {
+            message.text = TimestampTexts.SetTimestamp(message);
+        }
+    }
+
+    [HarmonyPatch(typeof(MSGConversation), "SendMessageChain")]
+    public class SendMessageChainPatch
+    {
+        static void Prefix(MessageChain messages, float initialDelay = 0f, bool notify = true, bool network = true)
+        {
+            for (int i = 0; i < messages.Messages.Count; i++)
+                messages.Messages[i] = TimestampTexts.SetTimestamp(messages.Messages[i], Message.ESenderType.Other);
+        }
+    }
+
+    [HarmonyPatch(typeof(MSGConversation), "MoveToTop")]
+    public class MoveToTopPatch
+    {
+        static bool Prefix(MSGConversation __instance)
+        {
+            string name = __instance?.sender?.fullName ?? null;
+            if (string.IsNullOrEmpty(name) || !TimestampTexts.BlockMove.TryGetValue(name, out _)) 
+                return true;
+
+            TimestampTexts.BlockMove.Remove(name);
+            return false;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: MelonInfo(typeof(Timestamp.TimestampTexts), "Timestamp Texts", "1.0.0", "GuysWeForgotDre")]
+[assembly: MelonGame("TVGS", "Schedule I")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Timestamp")]
+[assembly: AssemblyDescription("Adds timestamp and day label to text messages in Schedule One")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Timestamp")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cc60df14-d512-4596-a069-db9075541120")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
